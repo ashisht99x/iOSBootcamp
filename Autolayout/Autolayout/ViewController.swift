96c2213385986043a9c74f38d1d9dd986238e91e
Ashish Tyagi <ashisht@thoughtworks.com>
2015-05-26 00:55:30 +0530

    Autolayout with button and header

---
+++ Autolayout/Autolayout/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by Ashish Tyagi on 25/05/15.
+//  Copyright (c) 2015 Ashish Tyagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var headerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttonTapped(sender: AnyObject) {
+        
+    }
+
+}
+
